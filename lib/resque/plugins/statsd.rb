3a4f52133f55df475faa5531b20837af80965cb5
Philip Cristiano <philipcristiano@gmail.com>
2012-12-31 09:55:57 -0500

    job: Add counts and timing to queue.job

--- lib/resque/plugins/statsd.rb
+++ lib/resque/plugins/statsd.rb
@@ -3,33 +3,28 @@ module Resque
     module Statsd
 
       def after_enqueue_statsd(*args)
-        $resque_statsd.increment("#{@queue}.enqueued")
-        $resque_statsd.increment("#{self}.enqueued")
+        $resque_statsd.increment("#{@queue}.#{self}.enqueued")
         $resque_statsd.increment("total.enqueued")
       end
 
       def before_perform_statsd(*args)
-        $resque_statsd.increment("#{@queue}.started")
-        $resque_statsd.increment("#{self}.started")
+        $resque_statsd.increment("#{@queue}.#{self}.started")
         $resque_statsd.increment("total.started")
       end
 
       def after_perform_statsd(*args)
-        $resque_statsd.increment("#{@queue}.finished")
-        $resque_statsd.increment("#{self}.finished")
+        $resque_statsd.increment("#{@queue}.#{self}.finished")
         $resque_statsd.increment("total.finished")
       end
 
       def on_failure_statsd(*args)
-        $resque_statsd.increment("#{@queue}.failed")
-        $resque_statsd.increment("#{self}.failed")
+        $resque_statsd.increment("#{@queue}.#{self}.failed")
         $resque_statsd.increment("total.failed")
       end
 
       def around_perform_statsd(*args)
         timing = Benchmark.measure { yield }
-        $resque_statsd.timing("#{@queue}.processed", (timing.real * 1000.0).round)
-        $resque_statsd.timing("#{self}.processed", (timing.real * 1000.0).round)
+        $resque_statsd.timing("#{@queue}.#{self}.processed", (timing.real * 1000.0).round)
       end
 
     end
