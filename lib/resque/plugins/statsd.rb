e52fa542e6fad2956eec8661e6da3f44caff897c
Philip Cristiano <philipcristiano@gmail.com>
2012-12-31 09:54:53 -0500

    Remove trailing whitespace

--- lib/resque/plugins/statsd.rb
+++ lib/resque/plugins/statsd.rb
@@ -7,25 +7,25 @@ module Resque
         $resque_statsd.increment("#{self}.enqueued")
         $resque_statsd.increment("total.enqueued")
       end
-      
+
       def before_perform_statsd(*args)
         $resque_statsd.increment("#{@queue}.started")
         $resque_statsd.increment("#{self}.started")
         $resque_statsd.increment("total.started")
       end
-      
+
       def after_perform_statsd(*args)
         $resque_statsd.increment("#{@queue}.finished")
         $resque_statsd.increment("#{self}.finished")
         $resque_statsd.increment("total.finished")
       end
-      
+
       def on_failure_statsd(*args)
         $resque_statsd.increment("#{@queue}.failed")
         $resque_statsd.increment("#{self}.failed")
         $resque_statsd.increment("total.failed")
       end
-      
+
       def around_perform_statsd(*args)
         timing = Benchmark.measure { yield }
         $resque_statsd.timing("#{@queue}.processed", (timing.real * 1000.0).round)
